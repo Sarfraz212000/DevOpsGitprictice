c32aa1ed50e6320be731bfd729b7af46376aa143
Sarfraz <sarfrazprogrammer@gmail.com>
2023-10-16 11:44:43 +0530

    file commited

---
+++ src/main/java/in/saffu/GitCommandApplication.java
@@ -0,0 +1,13 @@
+package in.saffu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCommandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/saffu/GitCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package in.saffu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
